36f86a5c8411e07848bb3e714b1755068868c78e
Adam Ringhede <adamringhede@live.com>
2024-09-27 17:12:19 +0200

    update to hology core 0.0.53

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import hologyBuild from '@hology/vite-plugin'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 1600,
+  esbuild: {
+    target: "es2020",
   },
   plugins: [
+    hologyBuild(),
     react({
       babel: {
         plugins: [
-          ["@babel/plugin-proposal-decorators", { version: "2022-03" }],
+          ["@babel/plugin-proposal-decorators", { version: "2023-11" }],
+          ["module:@preact/signals-react-transform"],
         ],
       },
-  }),
+    }),
   ],
 })
