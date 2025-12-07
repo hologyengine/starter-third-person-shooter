ad3013597fc5032d5c82d3ea64b10fc26b84855f
Adam <adamringhede@live.com>
2025-12-07 17:01:27 +0100

    update hology

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import { HologyScene } from '@hology/react'
 import shaders from './shaders'
 import actors from './actors'
+import components from './components';
 import Game from './services/game'
 
 function App() {
   return (
-    <HologyScene gameClass={Game} sceneName='main' dataDir='data' shaders={shaders} actors={actors}>
+    <HologyScene gameClass={Game} sceneName='main' dataDir='data' shaders={shaders} actors={actors} components={components}>
     </HologyScene>
   );
 }
