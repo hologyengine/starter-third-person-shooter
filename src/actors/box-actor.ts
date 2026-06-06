d3313f19cee1052436eaaf3dc06fd936a48cbdc7
Adam <adamringhede@live.com>
2026-06-06 21:59:15 +0200

    replicate boxes

--- src/actors/box-actor.ts
+++ src/actors/box-actor.ts
@@ -1,14 +1,17 @@
 import { BoxCollisionShape, PhysicalShapeMesh } from "@hology/core"
-import { Actor, BaseActor, PhysicsBodyType } from "@hology/core/gameplay"
+import { Actor, BaseActor, inject, PhysicsBodyType, PhysicsSystem } from "@hology/core/gameplay"
 import { MeshComponent } from "@hology/core/gameplay/actors"
+import { NetActorRole, RunOnAll } from "@hology/core/gameplay/net"
 import { Parameter } from "@hology/core/shader/parameter"
-import { Color, MeshStandardMaterial, Vector3 } from "three"
+import { Color, MeshStandardMaterial, Quaternion, Vector3 } from "three"
 import { RoundedBoxGeometry } from "three/examples/jsm/geometries/RoundedBoxGeometry"
 
-@Actor()
+@Actor({replicate: true})
 class BoxActor extends BaseActor {
   @Parameter()
   private color: Color = new Color(0x0000ff)
+
+  private physics = inject(PhysicsSystem)
 
   private mesh = this.attach(MeshComponent<PhysicalShapeMesh>, {
     object: new PhysicalShapeMesh(
@@ -32,6 +35,28 @@ class BoxActor extends BaseActor {
     this.mesh.object.receiveShadow = true
     this.mesh.object.geometry.translate(0, 0.5, 0)
   }
+
+  onLateUpdate(deltaTime: number): void {
+    if (this.netRole === NetActorRole.authority) {
+      this.clientSyncTransform(this.position, this.quaternion)
+    } else if (this.hasSyncedUpdate) {
+      this.position.lerp(this.latestPosition, 0.2)
+      this.quaternion.slerp(this.latestRotation, 0.2)
+      this.physics.updateActorTransform(this)
+    }
+  }
+
+  // wish there was a 
+  @RunOnAll(false)
+  clientSyncTransform(position: Vector3, quaternion: Quaternion) {
+    this.latestPosition.copy(position)
+    this.latestRotation.copy(quaternion)
+    this.hasSyncedUpdate = true
+  }
+
+  private hasSyncedUpdate = false
+  private latestPosition = new Vector3()
+  private latestRotation = new Quaternion()
 }
 
 export default BoxActor
